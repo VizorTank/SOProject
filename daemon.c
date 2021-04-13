023bad7b9a02a0822f1422c3375ee0ab863dfe97
VizorTank <vizortank@wp.pl>
2021-04-13 11:27:38 +0200

    Daemon

--- daemon.c
+++ daemon.c
@@ -9,6 +9,29 @@
 #include <string.h>
 #include <dirent.h>
 
+int advanced = 0;
+int sleep_time = 0;
+
+static void signal_handler(int signo)
+{
+	if (signo == SIGUSR1)
+	{
+		//continue;
+		printf("SIGNUSR1");
+	}
+	else if (signo == SIGUSR2)
+	{
+		sleep(sleep_time);
+		printf("SIGNUSR2");
+	}
+	else
+	{
+		fprintf(stderr, "Nieoczekiwany sygnal!\n");
+		exit (EXIT_FAILURE);
+	}
+	exit (EXIT_SUCCESS);
+}
+
 void printdir(char *dir, char *s)
 {
 	DIR *dp;
@@ -30,13 +53,15 @@ void printdir(char *dir, char *s)
 			if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
 				continue;
 			
-			//printf("%s \n", entry->d_name);
+			//printf("Folder: %s \n", entry->d_name);
+			syslog(LOG_NOTICE, "Folder: %s \n", entry->d_name);
 			printdir(entry->d_name, s);
 		}
 		else
 		{
 			if (strstr(entry->d_name, s) != NULL)
-				printf("%s \n", entry->d_name);
+				syslog(LOG_NOTICE, "Plik: %s Wzorzec: %s\n", entry->d_name, s);
+				//printf("Plik: %s \n", entry->d_name);
 		}
 	}
 	chdir("..");
@@ -48,25 +73,66 @@ void printdir(char *dir, char *s)
 int main(int argc, char **argv)
 {
 
-	/*
+		
 	int d = daemon(0, 0);
 	if (d < 0)
+	{
+		
+	
+		
 		printf("NO");
+	}
+	
+	if (signal (SIGUSR1, signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "Nie mozna obsluzyc sygnalu SIGUSR1!");
+		exit (EXIT_FAILURE);
+	}
+	if (signal (SIGUSR2, signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "Nie mozna obsluzyc sygnalu SIGUSR2!");
+		exit (EXIT_FAILURE);
+	}
 	
 	openlog("test", LOG_PID, LOG_DAEMON);
 	
+	
+	/*
 	syslog(LOG_NOTICE, "T1");
 	sleep(20);
 	syslog(LOG_NOTICE, "T2");
-	closelog();
-	
-	return EXIT_SUCCESS;
 	*/
-	if (argc < 2)
-		printdir("/", "bc");
-	else
-		printdir("/", argv[1]);
 	
+	while(1)
+	{
+		if (argc < 2)
+			printdir("/", "bc");
+		else
+		{
+			int arguments = 0;
+			
+			if (argv[1 + arguments] == "-v")
+			{
+				advanced = 1;
+				arguments += 1;
+			}
+			else
+				advanced = 2;
+			
+			if (argv[1 + arguments] == "-t")
+			{
+				sleep_time = atoi(argv[2 + arguments]);
+				arguments += 2;
+			}
+			else
+				sleep_time = 1000;
+			
+			
+			printdir("/", argv[1 + arguments]);
+		}
+	}
 	
+	closelog();
+	return EXIT_SUCCESS;
 	return 0;
 }
