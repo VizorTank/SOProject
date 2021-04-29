ffe4fac7e3c0f9d713dae4850baf4ee2eece4337
VizorTank <36300779+VizorTank@users.noreply.github.com>
2021-04-29 16:01:05 +0200

    New functions

--- daemon.c
+++ daemon.c
@@ -22,10 +22,10 @@ int gid;
 static void signal_handler(int signo)
 {
 	// Normal signal handler
-	
+
 	if (advanced)
 		syslog(LOG_NOTICE, "Odebrano sygnal %d", signo);
-	
+
 	if (signo == SIGUSR1)
 	{
 		siglongjmp(jump, 1);
@@ -37,9 +37,9 @@ static void signal_handler(int signo)
 	else
 	{
 		syslog(LOG_NOTICE, "Nieoczekiwany sygnal!");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-	exit (EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 static void signal_handler_controller(int signo)
@@ -48,23 +48,23 @@ static void signal_handler_controller(int signo)
 
 	if (advanced)
 		syslog(LOG_NOTICE, "Kontroler odebral sygnal %d", signo);
-	
+
 	if (signo == SIGUSR1)
 	{
-		kill (-gid, signo);
+		kill(-gid, signo);
 	}
 	else if (signo == SIGUSR2)
 	{
-		kill (-gid, signo);
+		kill(-gid, signo);
 	}
 	else
 	{
 		syslog(LOG_NOTICE, "Nieoczekiwany sygnal!");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 }
 
-char* concat(const char *s1, const char *s2)
+char *concat(const char *s1, const char *s2)
 {
 	// Merge path
 
@@ -84,7 +84,7 @@ void find_files(char *path, char **file, const int start, const int arg)
 	struct stat statbuf;
 	int ret = 1;
 	DIR *dir;
-	
+
 	// Error opening directory
 	if ((dir = opendir(path)) == NULL)
 	{
@@ -92,7 +92,7 @@ void find_files(char *path, char **file, const int start, const int arg)
 			syslog(LOG_NOTICE, "Nie mozna otworzyc %s", path);
 		return;
 	}
-	
+
 	while ((entry = readdir(dir)) != NULL)
 	{
 		// Directory handler
@@ -101,26 +101,25 @@ void find_files(char *path, char **file, const int start, const int arg)
 		// Error checking stats
 		if (lstat(new_path, &statbuf) == -1)
 			continue;
-		
+
 		if (S_ISDIR(statbuf.st_mode))
 		{
-			if (!strcmp(".", entry->d_name)
-					|| !strcmp("..", entry->d_name))
+			if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name))
 				continue;
-			
+
 			if (advanced)
 				syslog(LOG_NOTICE, "Obsluzenie folderu %s", new_path);
-			
+
 			// Recursive search
 			find_files(new_path, file, start, arg);
 		}
 		else if (S_ISREG(statbuf.st_mode))
 		{
 			// File Handler
-			
+
 			if (advanced)
 				syslog(LOG_NOTICE, "Obsluzenie pliku %s", new_path);
-			
+
 			// Loop through patterns
 			int i;
 			for (i = start; i < arg; i++)
@@ -140,9 +139,51 @@ void find_files(char *path, char **file, const int start, const int arg)
 	return;
 }
 
+int create_forks(int forks)
+{
+	// Creates children and controller
+
+	int i, f = 0;
+	for (i = 0; i < forks; i++)
+	{
+		f = fork();
+		if (f == 0)
+			break;
+
+		if (i == 0)
+			gid = f;
+
+		setpgid(f, gid);
+	}
+	return f;
+}
+
+void create_sig_handler(int f)
+{
+	// Creates signal handlers
+
+	void (*fun_sig_han)(int);
+	if (f == 0)
+		fun_sig_han = &signal_handler;
+	else
+		fun_sig_han = &signal_handler_controller;
+
+	// Child or parent
+	if (signal(SIGUSR1, *fun_sig_han) == SIG_ERR)
+	{
+		syslog(LOG_NOTICE, "Nie mozna obsluzyc sygnalu SIGUSR1!");
+		exit(EXIT_FAILURE);
+	}
+	if (signal(SIGUSR2, *fun_sig_han) == SIG_ERR)
+	{
+		syslog(LOG_NOTICE, "Nie mozna obsluzyc sygnalu SIGUSR2!");
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main(int argc, char **argv)
 {
-	//Default
+	//Default arguments
 	int sleep_time = SLEEP_TIME;
 	int arguments = 0;
 	int forks = 0;
@@ -150,7 +191,7 @@ int main(int argc, char **argv)
 
 	char *dir = DIRECTORY;
 	char *log = LOG;
-	
+
 	// Argument handler
 	// Directory
 	if (argc - arguments >= 3 && !strcmp(argv[1 + arguments], "-d"))
@@ -172,29 +213,29 @@ int main(int argc, char **argv)
 		log = argv[2 + arguments];
 		arguments += 2;
 	}
-	
+
 	// Sleep time
 	if (argc - arguments >= 3 && !strcmp(argv[1 + arguments], "-t"))
 	{
 		sleep_time = atoi(argv[2 + arguments]);
 		arguments += 2;
 	}
-	
+
 	// Advanced
 	if (argc - arguments >= 2 && !strcmp(argv[1 + arguments], "-v"))
 	{
 		advanced = 1;
 		arguments += 1;
 	}
-	
+
 	// Too few arguments
 	if (argc - arguments <= 1)
 	{
 		fprintf(stderr, "Poprawne uzycie komendy\n");
 		fprintf(stderr, "%s [-d folder] [-f liczba_dzieci] [-l logi] [-t czas] [-v] wzorzec [wzorce ...]\n", argv[0]);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-	
+
 	// Daemon
 	int d = daemon(0, 0);
 	if (d < 0)
@@ -203,46 +244,19 @@ int main(int argc, char **argv)
 	// Forks
 	if (forks != 0)
 	{
-		int i;
-		for (i = 0; i < forks; i++)
-		{
-			f = fork();
-			if (f == 0)
-				break;
-			
-			if (i == 0)
-				gid = f;
-			
-			setpgid(f, gid);
-		}
+		f = create_forks(forks);
 	}
 
 	openlog(log, LOG_PID, LOG_DAEMON);
 
 	// Signal handlers
-	void (*fun_sig_han)(int);
-	if (f == 0)
-		fun_sig_han = &signal_handler;
-	else
-		fun_sig_han = &signal_handler_controller;
-	
-	// Child or parent
-	if (signal (SIGUSR1, *fun_sig_han) == SIG_ERR)
-	{
-		syslog(LOG_NOTICE, "Nie mozna obsluzyc sygnalu SIGUSR1!");
-		exit (EXIT_FAILURE);
-	}
-	if (signal (SIGUSR2, *fun_sig_han) == SIG_ERR)
-	{
-		syslog(LOG_NOTICE, "Nie mozna obsluzyc sygnalu SIGUSR2!");
-		exit (EXIT_FAILURE);
-	}
-	
+	create_sig_handler(f);
+
 	// Program
 	if (f == 0)
 	{
 		// Children
-		while(1)
+		while (1)
 		{
 			// Signals
 			int j = sigsetjmp(jump, 1);
@@ -250,14 +264,14 @@ int main(int argc, char **argv)
 			{
 				if (advanced)
 					syslog(LOG_NOTICE, "Obudzenie sie");
-				
+
 				// Searching
 				find_files(dir, argv, arguments + 1, argc);
 			}
 
 			if (advanced)
 				syslog(LOG_NOTICE, "Uspienie sie");
-			
+
 			sleep(sleep_time);
 		}
 	}
@@ -265,7 +279,7 @@ int main(int argc, char **argv)
 	{
 		// Controller
 		syslog(LOG_NOTICE, "Controller");
-		while(1)
+		while (1)
 			pause();
 	}
 
